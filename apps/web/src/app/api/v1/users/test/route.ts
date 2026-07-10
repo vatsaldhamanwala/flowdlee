24ac3a31d99a81ff776645e926718ddeec5b7ed1
Vatsal Dhamanwala <dhamanwalavatsal@gmail.com>
2026-07-11 01:14:29 +0530

    testing live git hook connection

--- apps/web/src/app/api/v1/users/test/route.ts
+++ apps/web/src/app/api/v1/users/test/route.ts
@@ -1,12 +1,12 @@
 import { USER } from '@/constants/api-response-error-messages';
-import { findUserByEmail } from '@/modules/users/user.service';
+import { findUserById } from '@/modules/users/user.service';
 import { successResponse } from '@/server/helpers/api-response';
 import { handleApiError } from '@/server/helpers/handle-api-error';
 import { StatusCodes } from 'http-status-codes';
 
 export async function GET() {
   try {
-    const usersWithEmailExist = await findUserByEmail('test@flowdlee.com');
+    const usersWithEmailExist = await findUserById('1'); // Replace '1' with the actual user ID you want to test
 
     return successResponse(
       {
